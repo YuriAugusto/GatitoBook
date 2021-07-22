e17c32261ac74bd731fe5fb5235d7e2b448f7c70
YuriAugusto <yuri.komesu@uni9.edu.br>
2021-07-22 11:14:45 -0300

    Parte 1 Aula 9

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',//se a url for vazia ele é redirecionado a home
+    pathMatch: 'full',//faz o tratamento de retirar os espaços, ex: se o usuário digitar a url: google .com, com o pathMath: 'full' a url será lida assim: google.com
+    redirectTo: 'home',//assim que ele acessar ele já irá para home
+  },
+  {
+    path: 'home',
+    loadChildren:() => import('./home/home.module').then((m) => m.HomeModule),
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
